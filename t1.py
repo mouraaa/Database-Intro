7785979721b6bb24c072c98d29775c00bafc2e51
Amr Abedo <codemouraa@gmail.com>
2020-09-16 22:36:52 -0400

    entry code #2

--- t1.py
+++ t1.py
@@ -5,21 +5,22 @@ try:
 	#connect to the database
 	connect = psycopg2.connect(
 			host = '127.0.0.1', 
-			database = 'name_of_your_database', 
+			database = 'name_of_database', 
 			user = 'postgres', 
-			password = 'postegre_password')
+			password = 'postgres_password')
 
 	#create a cursor object to use queries
 	cursor = connect.cursor()
 
 	#execute queries
-	cursor.execute('create table t1(id int, name varchar(10));')
+	# cursor.execute('drop table t1;')
+	cursor.execute('create table t1(name varchar(10), age int);')
 
 	while True:
 		name = input("Please enter the user's first name: ")
 		age = input(f'How old is {name}: ')
-		cursor.execute('Insert into t1(id,name) values (%s, %s);', (int(age), name))
-		answer = input("Do you have anyone else to add? [y/n]")
+		cursor.execute('Insert into t1(name,age) values (%s, %s);', (name, int(age)))
+		answer = input("Do you have anyone else to add? [y/n]: ")
 		if answer == 'y' or answer == 'yes':
 			continue
 		elif answer == 'n' or answer == 'no':
@@ -29,10 +30,17 @@ try:
 
 	cursor.execute('Select * from t1;')
 	records = cursor.fetchall()
-	print(tabulate(records, headers=['ID', 'Name'], tablefmt='psql')) 
+	#print with table
+	print(tabulate(records, headers=['Name', 'Age'], tablefmt='psql')) 
+
+	#print without table
+	# print("AGE:            NAME:")
+	# print('---------------------')
+	# for r in records:
+	# 	print(f'AGE: {r[0]}        NAME: {r[1]}' ) 
 
 except:
-	print("Something went wrong while connecting or quering the database")
+	print("Something went wrong while connecting or querying the database")
 
 finally:
 	connect.commit() 
